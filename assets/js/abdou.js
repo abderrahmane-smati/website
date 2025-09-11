b70b413b64105ba47cef66b82cf17f91f776d3d6
Abderrahmane Smati <abdousmati2001@gmail.com>
2025-09-11 09:47:14 +0100

    ...

--- assets/js/abdou.js
+++ assets/js/abdou.js
@@ -88,8 +88,8 @@ window.addEventListener('mousemove', () => {
 goTopBtn.addEventListener("click", function () {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    this.blur();  // force la perte du focus/hover pour les écrans tactiles
-    alert('click');
+    window.focus();  // force la perte du focus/hover pour les écrans tactiles
+    alert('focus');
 });
 // ***************************************** GoTop Button (end)
 
