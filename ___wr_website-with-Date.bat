4415b7565a678865522b09411f75077a54cfcea8
Abderrahmane Smati <abdousmati2001@gmail.com>
2025-09-14 11:16:01 +0100

    ...

--- ___wr_website-with-Date.bat
+++ ___wr_website-with-Date.bat
@@ -3,4 +3,8 @@
 for /f %%q in ("%~dp0.") do set CurrDirName=%%~nxq
 echo %CurrDirName%
 
-e:\tools\winrar\WINRAR a -hpHamdi67 _%CurrDirName%_%date:~6,4%%date:~3,2%%date:~0,2% @___wr_website_Files.txt
+set winrar_chemin=
+if exist e:\tools\winrar\WINRAR.exe set winrar_chemin=e:\tools\winrar\
+if exist d:\tools\winrar\WINRAR.exe set winrar_chemin=d:\tools\winrar\
+
+%winrar_chemin%WINRAR a -hpHamdi67 _%CurrDirName%_%date:~6,4%%date:~3,2%%date:~0,2% @___wr_website_Files.txt
